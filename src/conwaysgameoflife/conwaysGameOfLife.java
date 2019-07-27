ba9dd015a6210970bda5ced9a6c32ff4a0499e81
Hus <Hus@Huseyin>
2019-07-27 20:37:15 +0300

    Testing rulesOfLife Method with main method

--- src/conwaysgameoflife/conwaysGameOfLife.java
+++ src/conwaysgameoflife/conwaysGameOfLife.java
@@ -5,18 +5,62 @@
  */
 package conwaysgameoflife;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author Hus
  */
 public class conwaysGameOfLife
 {
-    public static boolean[][] tempBoard = new boolean[1][1];
-    public static boolean[][] ReadFile(){return tempBoard;}
-    public static void printBoard(boolean[][] matrix){}
-    public static boolean[][] nextGeneration(){return tempBoard;}
-    public static boolean rulesOfLife(int numberOfNeighbors, boolean isAlive) {return false;}
-    private static int numberOfNeighbors(boolean[][] board, int row, int col) {return 0;}
-    private static boolean inBound(boolean[][] board, int row, int col){return false;}
+    public boolean[][] tempBoard = new boolean[1][1];
+    
+    public boolean[][] ReadFile() throws FileNotFoundException{
+        Scanner inputfile;
+        boolean[][] arr = new boolean[10][10];
+        File file = new File("board.txt");
+	inputfile = new Scanner(file);
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr[0].length; j++)
+            {
+            	arr[i][j] = isTrue4ReadFile(inputfile);
+            }
+        }
+        return arr;
+    }
+    public void printBoard(boolean[][] board)
+    {
+        String cell = "";
+        for(boolean[] i : board){
+            for(boolean val : i)
+                if(val)
+                    cell += "1 ";
+                else
+                    cell += "0 ";
+            cell += "\n";
+        }
+        System.out.println(cell);
+    }
+    
+    public boolean[][] nextGeneration(){
+        return tempBoard;
+    }
+    
+    public boolean rulesOfLife(int numberOfNeighbors, boolean isAlive) {
+        return (isAlive && (numberOfNeighbors == 2 || numberOfNeighbors == 3)) || (!isAlive && numberOfNeighbors == 3);
+    }
+    
+    private int numberOfNeighbors(boolean[][] board, int row, int col) {return 0;}
+    private boolean inBound(boolean[][] board, int row, int col){return false;}
+    
+    
+    
+    public boolean isTrue4ReadFile(Scanner inputfile)
+    {
+        return inputfile.hasNextInt() && inputfile.nextInt() != 0;
+    }
     
 }
